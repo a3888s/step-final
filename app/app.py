9e69ed1123e8bce95d558a027cf5122bcd7c6a61
A3888S <sowdirect@gmail.com>
2024-12-20 21:19:34 +0000

    Додавання тегу під час виконання CI/CD через envsubst

--- app/app.py
+++ app/app.py
@@ -1,4 +1,8 @@
+import os
 from http.server import HTTPServer, BaseHTTPRequestHandler
+
+# Отримання тега з оточення
+DOCKER_TAG = os.getenv("DOCKER_TAG")
 
 # Клас для обробки HTTP-запитів
 class SimpleHandler(BaseHTTPRequestHandler):
@@ -7,7 +11,7 @@ class SimpleHandler(BaseHTTPRequestHandler):
         if self.path == "/":  # Якщо запитаний кореневий маршрут
             self.send_response(200)  # Відправка відповіді зі статусом 200 (OK)
             self.end_headers()  # Завершення заголовків відповіді
-            html_content = """  # HTML-контент для відповіді
+            html_content = f"""  # HTML-контент для відповіді
                 <!DOCTYPE html>
                 <html>
                 <head>
@@ -28,7 +32,7 @@ class SimpleHandler(BaseHTTPRequestHandler):
                     </style>
                 </head>
                 <body>
-                    <h1>StepFinalProject v 4.0.1</h1>
+                    <h1>StepFinalProject {DOCKER_TAG}</h1>
                 </body>
                 </html>
             """
@@ -39,5 +43,5 @@ class SimpleHandler(BaseHTTPRequestHandler):
 # Основна частина програми
 if __name__ == "__main__":
     server = HTTPServer(("0.0.0.0", 8080), SimpleHandler)  # Ініціалізація сервера на порту 8080
-    print("StepFinalProject v 4.0.1")  # Виведення інформації про запуск у консоль
+    print(f"StepFinalProject {DOCKER_TAG}")  # Виведення інформації про запуск у консоль
     server.serve_forever()  # Запуск сервера для обробки запитів

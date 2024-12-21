d3efed3d755be81108a01cbb0b805323f54454b8
A3888S <sowdirect@gmail.com>
2024-12-21 15:39:32 +0000

    Додано pipilene для збірки image середовища prod

--- app/app.py
+++ app/app.py
@@ -1,8 +1,4 @@
-import os
 from http.server import HTTPServer, BaseHTTPRequestHandler
-
-# Отримання тега з оточення
-DOCKER_TAG = os.getenv("DOCKER_TAG")
 
 # Клас для обробки HTTP-запитів
 class SimpleHandler(BaseHTTPRequestHandler):
@@ -11,7 +7,7 @@ class SimpleHandler(BaseHTTPRequestHandler):
         if self.path == "/":  # Якщо запитаний кореневий маршрут
             self.send_response(200)  # Відправка відповіді зі статусом 200 (OK)
             self.end_headers()  # Завершення заголовків відповіді
-            html_content = f"""  # HTML-контент для відповіді
+            html_content = """  # HTML-контент для відповіді
                 <!DOCTYPE html>
                 <html>
                 <head>
@@ -32,17 +28,16 @@ class SimpleHandler(BaseHTTPRequestHandler):
                     </style>
                 </head>
                 <body>
-                    <h1>StepFinalProject {DOCKER_TAG}</h1>
+                    <h1>StepFinalProject v 0.0.1</h1>
                 </body>
                 </html>
             """
             self.wfile.write(html_content.encode("utf-8"))  # Відправка HTML-контенту
         else:
             self.send_response(404)  # Відправка відповіді зі статусом 404 (Not Found)
-            self.end_headers()
 
 # Основна частина програми
 if __name__ == "__main__":
     server = HTTPServer(("0.0.0.0", 8080), SimpleHandler)  # Ініціалізація сервера на порту 8080
-    print(f"StepFinalProject {DOCKER_TAG}")  # Виведення інформації про запуск у консоль
+    print("StepFinalProject v 4.0.1")  # Виведення інформації про запуск у консоль
     server.serve_forever()  # Запуск сервера для обробки запитів
